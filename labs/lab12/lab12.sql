222c10411c5dceae3197f98d1164e432dd7ea01b
lnvitesace <lnvitesace@gmail.com>
2023-09-11 14:55:33 +0800

    hw09

--- labs/lab12/lab12.sql
+++ labs/lab12/lab12.sql
@@ -2,20 +2,31 @@
 
 
 CREATE TABLE bluedog AS
-  SELECT color, pet from students where color = "blue" and pet = "dog";
+  SELECT color, pet
+  FROM students
+  WHERE color = "blue" AND pet = "dog";
 
 CREATE TABLE bluedog_songs AS
-  SELECT color, pet, song from students where color = "blue" and pet = "dog";
+  SELECT color, pet, song
+  FROM students
+  WHERE color = "blue" AND pet = "dog";
 
 
 CREATE TABLE smallest_int AS
-  SELECT time, smallest from students where smallest > 2 order by smallest limit 20;
+  SELECT time, smallest
+  FROM students
+  WHERE smallest > 2
+  ORDER BY smallest LIMIT 20;
 
 
 CREATE TABLE matchmaker AS
-  SELECT a.pet, a.song, a.color, b.color from students as a, students as b where a.pet = b.pet and a.song = b.song and a.time < b.time;
+  SELECT a.pet, a.song, a.color, b.color
+  FROM students AS a, students AS b
+  WHERE a.pet = b.pet AND a.song = b.song AND a.time < b.time;
 
 
 CREATE TABLE sevens AS
-  SELECT a.seven from students as a, numbers as b where a.number = 7 and b.'7' = 'True' and a.time = b.time;
+  SELECT a.seven
+  FROM students AS a, numbers AS b 
+  WHERE a.number = 7 AND b.'7' = 'True' AND a.time = b.time;
 
